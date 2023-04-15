e8b4e4b31a73322e176b23ea37736c916e4dde4c
Eliza Ciovica <elizaciovica0208@gmail.com>
2023-04-15 18:20:11 +0300

    Implemented Navigation DrawerLayoutActivity.kt

--- app/src/main/java/edu/licenta/uptconnect/HomeActivity.kt
+++ app/src/main/java/edu/licenta/uptconnect/HomeActivity.kt
@@ -1,11 +1,30 @@
 package edu.licenta.uptconnect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import edu.licenta.uptconnect.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : DrawerLayoutActivity() {
+
+    private val screenId: Int = 1
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setBinding()
+        initializeMenu(
+            binding.drawerLayout,
+            binding.navigationView,
+            screenId
+        )
     }
+
+    private fun setBinding() {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
 }
